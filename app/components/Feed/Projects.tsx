d09916d163a5a42dca3e1db2ae8a3b51b6337ed1
qirall79 <belfatmi79@gmail.com>
2024-05-04 22:54:58 +0100

    Remove unnecessary pagination and display loading state while fetching next items

--- app/components/Feed/Projects.tsx
+++ app/components/Feed/Projects.tsx
@@ -4,12 +4,13 @@ import { trpc } from "@/lib/trpcClient";
 import { IProject } from "@/lib/types";
 import { Project } from "./Project";
 import { ProjectSkeleton } from "../Skeletons/ProjectSkeleton";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 export const Projects = ({ query }: { query?: string }) => {
   const [page, setPage] = useState(0);
-  const searchParams = useSearchParams()
+  const skeletonRef = useRef<HTMLDivElement | null>(null)
+  const searchParams = useSearchParams();
   const projectsQuery = trpc.projects.getAll.useInfiniteQuery(
     { query },
     {
@@ -18,13 +19,12 @@ export const Projects = ({ query }: { query?: string }) => {
       },
     }
   );
-  const projects = projectsQuery.data?.pages[page]?.projects;
   const handleScroll = () => {
     projectsQuery.fetchNextPage();
     setPage(page + 1);
   };
 
-  if (projectsQuery.isLoading || projectsQuery.isFetchingNextPage)
+  if (projectsQuery.isLoading)
     return (
       <div className="flex flex-col grow space-y-6">
         <ProjectSkeleton />
@@ -35,10 +35,15 @@ export const Projects = ({ query }: { query?: string }) => {
 
   return (
     <div className="flex flex-col grow space-y-6">
-      {projects?.map((project: IProject) => {
-        return <Project key={project.id} project={project} />;
+      {projectsQuery.data?.pages.map((page: { projects: IProject[] }) => {
+        return page.projects?.map((project: IProject) => {
+          return <Project key={project.id} project={project} />;
+        });
       })}
-      {!searchParams.get("query") && projects && projects.length >= 5 && <button onClick={handleScroll}>Load more...</button>}
+      {projectsQuery.isFetchingNextPage && <ProjectSkeleton ref={skeletonRef} />}
+      {!projectsQuery.isFetchingNextPage && !searchParams.get("query") && projectsQuery.hasNextPage && (
+        <button onClick={handleScroll}>Load more...</button>
+      )}
     </div>
   );
 };
--- app/components/Skeletons/ProjectSkeleton.tsx
+++ app/components/Skeletons/ProjectSkeleton.tsx
@@ -1,9 +1,10 @@
 import { Skeleton } from "@nextui-org/react";
+import { MutableRefObject } from "react";
 
-export const ProjectSkeleton = () => {
+export const ProjectSkeleton = ({ref}: {ref?: MutableRefObject<any>}) => {
   return (
-    <div className="w-full flex items-center gap-3">
-      <div className="w-full flex flex-col gap-2 border border-slate-800 w-full p-6 rounded-xl">
+    <div ref={ref} className="w-full flex items-center gap-3">
+      <div className="w-full flex flex-col gap-2 border border-slate-800 p-6 rounded-xl">
         <Skeleton className="h-3 w-1/5 rounded-lg" />
         <Skeleton className="h-3 w-1/6 rounded-lg" />
         <Skeleton className="h-3 w-1/2 rounded-lg" />
